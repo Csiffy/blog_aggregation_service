305f5afdcee10d9a20970faae24fbae7221bb18c
Csiffy <csiffaryz@gmail.com>
2017-12-04 22:27:58 +0000

    loop to determine the html element

--- server.js
+++ server.js
@@ -18,8 +18,13 @@ const parse5 = require('parse5');
 const request = require('request');
 
 // Variable declaraion section
-// TODO Example page, need to implement with running parameter
-var url = "https://wordpress.com/learn-more/?v=blog" // It contains only rsd and rss
+// PLEASE MODIFY THESE PARAMETERS AS NEEDED
+// atom type url => https://github.com/FreshRSS/FreshRSS
+// rss type url => https://wordpress.com/learn-more/?v=blog
+var url = "https://wordpress.com/learn-more/?v=blog";
+var isDebugMode = false;
+
+
 
 var page_document = '';
 var page_html = '';
@@ -29,7 +34,6 @@ var page_body = '';
 var output = new Object();
 var atom_array = new Array();
 var rss_array = new Array();
-var isDebugMode = false;
 
 // Function section
 // This is a helper function for debuging, it is using the isDebugMode variable to control the console output
@@ -44,11 +48,10 @@ console.debug = function(args)
 // Array creation function which is checking the link type and push the url into the regarding array
 // It have two parameter, one is the exact link, and the other one is the link type
 function create_array(link, type) {
-  for (var f = 0; f < link.length; f++) {
-    array_item = link[f];
+  for (var j = 0; j < link.length; j++) {
+    array_item = link[j];
     if (array_item.name === "href") {
-      // TODO Final type will be atom+xml
-      if (type === "application/rsd+xml") {
+      if (type === "application/atom+xml") {
         atom_array.push(array_item.value);
       }
       else if (type === "application/rss+xml") {
@@ -71,37 +74,38 @@ request(url, function (err, res, body) {
   page_header = res.headers;
   const page_document = parse5.parse(body);
   console.debug(page_document);
-  // TODO kell var, mert lehet 0. es 1. item is a html
-  if (page_document.childNodes[1].tagName === "html") {
-    page_html = page_document.childNodes[1];
-  } else {
+  for (var f = 0; f < page_document.childNodes.length; f++) {
+    if (page_document.childNodes[f].nodeName === "html") {
+      page_html = page_document.childNodes[f];
+    }
+  }
+  if (page_html === 0) {
     console.log('Error, did not found html element');
     process.exit(-1);
   }
   console.debug('---------- Page HTML ----------');
   console.debug(page_html);
   console.debug('-------------------------------');
-  for(var h = 0; h < page_html.childNodes.length; h++) {
-    console.debug('page_html belseje', page_html.childNodes[h].tagName);
-    if (page_html.childNodes[h].tagName === 'head') {
-      page_head = page_html.childNodes[h].childNodes;
+  for(var g = 0; g < page_html.childNodes.length; g++) {
+    console.debug('page_html belseje', page_html.childNodes[g].tagName);
+    if (page_html.childNodes[g].tagName === 'head') {
+      page_head = page_html.childNodes[g].childNodes;
     }
-    if (page_html.childNodes[h].tagName === 'body') {
-      page_body = page_html.childNodes[h].childNodes;
+    if (page_html.childNodes[g].tagName === 'body') {
+      page_body = page_html.childNodes[g].childNodes;
     }
   }
   console.debug('----------- Page Head ---------');
   console.debug(page_head);
   console.debug('-------------------------------');
-  for(var i = 0; i < page_head.length;i++){
-    current_item = page_head[i];
+  for(var h = 0; h < page_head.length;h++){
+    current_item = page_head[h];
     if (current_item.nodeName === "link") {
-      second_state = current_item.attrs;
-      for (var k = 0; k < second_state.length; k++) {
-        namevalueitem = second_state[k];
-        // TODO please modify back in production: if (namevalueitem.name === "type" && (namevalueitem.value === "application/atom+xml" || namevalueitem.value === "application/rss+xml"))
-        if (namevalueitem.name === "type" && (namevalueitem.value === "application/rsd+xml" || namevalueitem.value === "application/rss+xml")) {
-          create_array(second_state, namevalueitem.value);
+      link_items = current_item.attrs;
+      for (var i = 0; i < link_items.length; i++) {
+        namevalue_item = link_items[i];
+        if (namevalue_item.name === "type" && (namevalue_item.value === "application/atom+xml" || namevalue_item.value === "application/rss+xml")) {
+          create_array(link_items, namevalue_item.value);
         }
       }
     }
